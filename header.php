fa70cc2f16c9e5cd92883e6b3cd470f4edcb40cd
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2024-12-17 00:42:40 +0300

    profil sayfaları düzenlendi

--- header.php
+++ header.php
@@ -140,26 +140,26 @@
                         </svg>
                     </button>
                     <div class="dropdown-menu group-dropdown-hover:visible lg:invisible !-right-4 !top-[85%] !left-auto z-10 hidden min-w-[14rem] whitespace-nowrap rounded-xl bg-white transition-all will-change-transform before:absolute before:-top-3 before:h-3 before:w-full group-dropdown-hover:opacity-100 dark:bg-jacarta-800 lg:absolute lg:grid lg:!translate-y-4 lg:py-4 lg:px-2 lg:opacity-0 lg:shadow-2xl" aria-labelledby="profileDropdown">
-                        <a href="#" class="flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors hover:bg-jacarta-50 hover:text-[#caa754] focus:text-[#caa754] dark:hover:bg-jacarta-600">
+                        <a href="/profil/hesabim.php" class="flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors hover:bg-jacarta-50 hover:text-[#caa754] focus:text-[#caa754] dark:hover:bg-jacarta-600">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="h-4 w-4 fill-jacarta-700 transition-colors dark:fill-white">
                                 <path fill="none" d="M0 0h24v24H0z"></path>
                                 <path d="M11 14.062V20h2v-5.938c3.946.492 7 3.858 7 7.938H4a8.001 8.001 0 0 1 7-7.938zM12 13c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z"></path>
                             </svg>
-                            <span class="mt-1 font-display text-sm text-jacarta-700 dark:text-white">My Profile</span>
+                            <span class="mt-1 font-display text-sm text-jacarta-700 dark:text-white">Hesabım</span>
                         </a>
-                        <a href="#" class="flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors hover:bg-jacarta-50 hover:text-[#caa754] focus:text-[#caa754] dark:hover:bg-jacarta-600">
+                        <a href="/profil/siparislerim.php" class="flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors hover:bg-jacarta-50 hover:text-[#caa754] focus:text-[#caa754] dark:hover:bg-jacarta-600">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="h-4 w-4 fill-jacarta-700 transition-colors dark:fill-white">
                                 <path fill="none" d="M0 0h24v24H0z" />
                                 <path d="M9.954 2.21a9.99 9.99 0 0 1 4.091-.002A3.993 3.993 0 0 0 16 5.07a3.993 3.993 0 0 0 3.457.261A9.99 9.99 0 0 1 21.5 8.876 3.993 3.993 0 0 0 20 12c0 1.264.586 2.391 1.502 3.124a10.043 10.043 0 0 1-2.046 3.543 3.993 3.993 0 0 0-3.456.261 3.993 3.993 0 0 0-1.954 2.86 9.99 9.99 0 0 1-4.091.004A3.993 3.993 0 0 0 8 18.927a3.993 3.993 0 0 0-3.457-.26A9.99 9.99 0 0 1 2.5 15.121 3.993 3.993 0 0 0 4 11.999a3.993 3.993 0 0 0-1.502-3.124 10.043 10.043 0 0 1 2.046-3.543A3.993 3.993 0 0 0 8 5.071a3.993 3.993 0 0 0 1.954-2.86zM12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                             </svg>
-                            <span class="mt-1 font-display text-sm text-jacarta-700 dark:text-white">Edit Profile</span>
+                            <span class="mt-1 font-display text-sm text-jacarta-700 dark:text-white">Siparişlerim</span>
                         </a>
                         <a href="#" class="flex items-center space-x-2 rounded-xl px-5 py-2 transition-colors hover:bg-jacarta-50 hover:text-[#caa754] focus:text-[#caa754] dark:hover:bg-jacarta-600">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="h-4 w-4 fill-jacarta-700 transition-colors dark:fill-white">
                                 <path fill="none" d="M0 0h24v24H0z" />
                                 <path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zM7 11V8l-5 4 5 4v-3h8v-2H7z"/>
                             </svg>
-                            <span class="mt-1 font-display text-sm text-jacarta-700 dark:text-white">Sign out</span>
+                            <span class="mt-1 font-display text-sm text-jacarta-700 dark:text-white">Çıkış Yap</span>
                         </a>
                     </div>
                 </div>
